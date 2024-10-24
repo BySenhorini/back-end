611432462d1939a20838bd03050fe020e859068f
Yasmin Senhorini <yasminsenhorine33@gmail.com>
2024-10-24 16:32:46 -0300

    Desafio Arrays

---
+++ Array/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] carros = new string[3];
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Digite o nome do carro a ser cadastrado:");
+    carros[i] = Console.ReadLine()!;
+
+}
+Console.WriteLine($"");
+Console.WriteLine($"O carro cadastradado e {carros[0]}, {carros[1]}, e {carros[2]} ");
+Console.WriteLine($"");
+
---
+++ Desafio01/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numeros = new int[6];
+int qtdPares = 0, qtdImpares = 0;
+
+// Lendo os 6 números inteiros
+Console.WriteLine("Digite 6 números inteiros:");
+for (int i = 0; i < 6; i++)
+{
+    Console.Write($"Número {i + 1}: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+// Contando a quantidade de números pares e ímpares
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+    {
+        qtdPares++;
+    }
+    else
+    {
+        qtdImpares++;
+    }
+}
+
+// Exibindo os resultados
+Console.WriteLine($"\nQuantidade de números pares: {qtdPares}");
+Console.WriteLine($"Quantidade de números ímpares: {qtdImpares}");
+
