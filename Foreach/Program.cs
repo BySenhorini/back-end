2ac1d80fba479ddf8f346f8d189a16966f2d3f34
Yasmin Senhorini <yasminsenhorine33@gmail.com>
2024-11-05 16:44:53 -0300

    atualizando a pasta backend

---
+++ Foreach/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string frutas = ("Melancias", "Carambola", "Maracuja", "Morango", "Limao");
+
+Console.WriteLine($"Sua sacola contem {frutas.Length} frutas!");
+
+foreach (string f in frutas)
+
+{
+    Console.WriteLine(f);
+}
